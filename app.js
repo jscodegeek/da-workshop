b4810d1541420034feeccb073a7617840e9d4798
Eugene Safronov <evgeniy.safronov@outlook.com>
2018-09-29 11:21:35 +0300

    git add express

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const {NODE_ENV, PORT} = process.env;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
