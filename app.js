e0406f219f7e77d04c869822885958ba4ec7c26c
Eugene Safronov <evgeniy.safronov@outlook.com>
2018-09-29 11:59:47 +0300

    progress

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
+const uuidv1 = require('uuid/v1');
+const _ = require('lodash');
+
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const bodyParser = require('body-parser');
 
 const {NODE_ENV, PORT} = process.env;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser.json());
+
+app.get('/hc', (req, res) => {
+  res.json({ status: 'OK'});
+});
+
+app.get('/cars', (req, res) => {
+  const cars = db.get('cars');
+  res.json(cars);
+});
+
+app.post('/cars', (req, res) => {
+  const { title, brand, number } = req.body;
+
+  const cars = db.get('cars')
+    .push({ id: uuidv1(), title, brand, number })
+    .write();
+
+  res.json(_.last(cars));
+});
+
+app.put('/cars/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({ status: 'OK'});
+});
+
+app.delete('/cars/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({ status: 'OK'});
+});
+
+app.get('/cars/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({ status: 'OK'});
+});
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
